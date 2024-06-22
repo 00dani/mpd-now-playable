3cb5db7528b67be4f2e49aa0c79f9ea2911733b3
Danielle McLean <dani@00dani.me>
2024-06-22 20:12:23 +1000

    Add field descriptions to the config schema :)

--- src/mpd_now_playable/config/model.py
+++ src/mpd_now_playable/config/model.py
@@ -11,8 +11,14 @@ __all__ = ("MpdConfig", "Config")
 
 @dataclass(frozen=True)
 class MpdConfig:
+	#: The password required to connect to your MPD instance, if you need one.
 	password: Optional[str] = optional()
+	#: The hostname or IP address of your MPD server. If you're running MPD
+	#: on your local machine, you don't need to configure this.
 	host: Host = Host("127.0.0.1")
+	#: The port on which to connect to MPD. Unless you're managing multiple MPD
+	#: servers on one machine for some reason, you probably haven't changed this
+	#: from the default port, 6600.
 	port: Port = Port(6600)
 
 
@@ -22,5 +28,8 @@ class Config:
 		default=URL("https://cdn.00dani.me/m/schemata/mpd-now-playable/config-v1.json"),
 		metadata=alias("$schema"),
 	)
+
+	#: A URL describing a cache service for mpd-now-playable to use. Supported
+	#: protocols are memory://, redis://, and memcached://.
 	cache: Optional[URL] = optional()
 	mpd: MpdConfig = field(default_factory=MpdConfig)
--- src/mpd_now_playable/config/schema.py
+++ src/mpd_now_playable/config/schema.py
@@ -3,13 +3,30 @@ from pathlib import Path
 from pprint import pp
 from typing import Any, Mapping
 
+from apischema import schema, settings
 from apischema.json_schema import JsonSchemaVersion, deserialization_schema
+from apischema.schemas import Schema
+from class_doc import extract_docs_from_cls_obj
 
 from .model import Config
 
 
+def field_base_schema(tp: type, name: str, alias: str) -> Schema | None:
+	desc_lines = extract_docs_from_cls_obj(tp).get(name, [])
+	if desc_lines:
+		print((tp, name, alias))
+		return schema(description=" ".join(desc_lines))
+	return None
+
+
+settings.base_schema.field = field_base_schema
+
+
 def generate() -> Mapping[str, Any]:
-	return deserialization_schema(Config, version=JsonSchemaVersion.DRAFT_7)
+	return deserialization_schema(
+		Config,
+		version=JsonSchemaVersion.DRAFT_7,
+	)
 
 
 def write() -> None:
