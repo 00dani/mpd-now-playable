3b7ddfa718fd0e9200284074bf01b427fb97796c
Danielle McLean <dani@00dani.me>
2024-06-28 14:14:24 +1000

    fix: handle missing environment variables more robustly

--- src/mpd_now_playable/config/load.py
+++ src/mpd_now_playable/config/load.py
@@ -1,12 +1,32 @@
+from collections.abc import Mapping
 from os import environ
+from typing import TypeVar
 
 from apischema import deserialize
+from boltons.iterutils import remap
 from pytomlpp import load
 from xdg_base_dirs import xdg_config_home
 
 from .model import Config
 
-__all__ = "loadConfig"
+__all__ = ("loadConfig",)
+K = TypeVar("K")
+V = TypeVar("V")
+
+
+# Sadly this is the kind of function that's incredibly easy to type statically
+# in something like TypeScript, but apparently impossible to type statically in
+# Python. Basically the TypeScript typing looks like this:
+#   type OmitNulls<T> = {[K in keyof T]?: OmitNulls<NonNullable<T[K]>>};
+#   function withoutNulls<T>(data: T): OmitNulls<T>;
+# But the Python type system (currently?) lacks mapped and index types, so you
+# can't recurse over an arbitrary type's structure as OmitNulls<T> does, as
+# well as conditional types so you can't easily define a "filtering" utility
+# type like NonNullable<T>. Python's type system also doesn't infer a
+# dictionary literal as having a structural type by default in the way
+# TypeScript does, of course, so that part wouldn't work anyway.
+def withoutNones(data: Mapping[K, V | None]) -> Mapping[K, V]:
+	return remap(data, lambda p, k, v: v is not None)
 
 
 def loadConfigFromFile() -> Config:
@@ -17,14 +37,14 @@ def loadConfigFromFile() -> Config:
 
 
 def loadConfigFromEnv() -> Config:
-	port = int(environ["MPD_PORT"]) if "MPD_PORT" in environ else None
-	host = environ.get("MPD_HOST")
-	password = environ.get("MPD_PASSWORD")
-	cache = environ.get("MPD_NOW_PLAYABLE_CACHE")
+	port = int(environ.pop("MPD_PORT")) if "MPD_PORT" in environ else None
+	host = environ.pop("MPD_HOST", None)
+	password = environ.pop("MPD_PASSWORD", None)
+	cache = environ.pop("MPD_NOW_PLAYABLE_CACHE", None)
 	if password is None and host is not None and "@" in host:
 		password, host = host.split("@", maxsplit=1)
 	data = {"cache": cache, "mpd": {"port": port, "host": host, "password": password}}
-	return deserialize(Config, data)
+	return deserialize(Config, withoutNones(data))
 
 
 def loadConfig() -> Config:
---
+++ stubs/boltons/iterutils.pyi
@@ -0,0 +1,23 @@
+from collections.abc import Callable, Mapping
+from typing import TypeAlias, TypeVar
+
+# Apparently you need Python 3.13 for type var defaults to work. But since this
+# is just a stub file, it's okay if they aren't supported at runtime.
+KIn = TypeVar("KIn")
+KOut = TypeVar("KOut", default=KIn)
+VIn = TypeVar("VIn")
+VOut = TypeVar("VOut", default=VIn)
+
+Path: TypeAlias = tuple[KIn, ...]
+
+# remap() is Complicated and really difficult to define a type for, so I'm not
+# surprised the boltons package doesn't try to type it for you. This particular
+# type declaration works fine for my use of the function, but it's actually
+# vastly more flexible than that - it'll accept any iterable as the root, not
+# just mappings, and you can provide "enter" and "exit" callables to support
+# arbitrary data structures.
+def remap(
+	root: Mapping[KIn, VIn],
+	visit: Callable[[Path[KIn], KIn, VIn], tuple[KOut, VOut] | bool],
+	reraise_visit: bool = False,
+) -> Mapping[KOut, VOut]: ...
